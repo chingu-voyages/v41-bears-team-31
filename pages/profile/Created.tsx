ad0a4e69b34c1bd1be1688a8ef0bf14110ad85bf
ChauTran73 <cmtran7393@gmail.com>
2022-11-02 16:31:15 +0700

    added category tags

--- pages/profile/Created.tsx
+++ pages/profile/Created.tsx
@@ -3,6 +3,8 @@ import React, { useEffect, useRef, useState } from 'react';
 import { PageLayout } from '../../components';
 import Card from '../../components/Common/Card';
 import mockData from '../../data/posts_dummy.json';
+import tagsData from '../../data/category_tags.json';
+import Button from '../../components/Common/Button';
 
 const Created: NextPage = () => {
   const [createdPosts, setCreatedPosts] = useState(null);
@@ -11,6 +13,18 @@ const Created: NextPage = () => {
     <PageLayout>
       <section className="container mx-auto ">
         <h1>Created Posts</h1>
+        <div className="mb-3 mt-3 flex flex-row">
+          {tagsData.map((tag) => {
+            return (
+              <div key={tag._id} className="mr-3">
+                <Button className="rounded-md border-solid p-2 shadow-lg">
+                  {tag.name}
+                </Button>
+              </div>
+            );
+          })}
+        </div>
+
         <div className="flex flex-col gap-x-1.5 gap-y-1.5 md:flex-row">
           {mockData.slice(0, 4).map((spook) => {
             return (
